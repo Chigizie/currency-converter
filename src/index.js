e51a37508b49f3f0df2278a083244e6cbdec4257
Chigoziem <chigoziemwell@gmail.com>
2024-01-01 14:25:43 +0100

    currency-converter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [fromcurr, setFromcurr] = useState("USD");
+  const [toCurr, SetTocurr] = useState("EUR");
+  const [rate, setRate] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(
+    function converter() {
+      const host = "api.frankfurter.app";
+
+      async function convert() {
+        const res = await fetch(
+          `https://${host}/latest?amount=${amount}&from=${fromcurr}&to=${toCurr}`
+        );
+        const data = await res.json();
+
+        setRate(data.rates[toCurr]);
+      }
+      if (fromcurr === toCurr || !amount) {
+        setRate(amount);
+        return;
+      }
+
+      convert();
+    },
+    [amount, fromcurr, toCurr]
+  );
+
+  return (
+    <div className="converter">
+      <Currency />
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(() => e.target.value)}
+      />
+      <select value={fromcurr} onChange={(e) => setFromcurr(e.target.value)}>
+        <option>EUR</option>
+        <option>USD</option>
+        <option>JPY</option>
+        <option>GBP</option>
+        <option>CAD</option>
+        <option>AUD</option>
+        <option>NGN</option>
+        <option>NZD</option>
+      </select>
+      <select value={toCurr} onChange={(e) => SetTocurr(e.target.value)}>
+        <option>EURO</option>
+        <option>USD</option>
+        <option>JPY</option>
+        <option>GBP</option>
+        <option>CAD</option>
+        <option>AUD</option>
+        <option>NGN</option>
+        <option>NZD</option>
+      </select>
+
+      <p>{rate}</p>
+    </div>
+  );
+}
+
+function Currency() {
+  return (
+    <div>
+      <h1>Currency Converter</h1>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
